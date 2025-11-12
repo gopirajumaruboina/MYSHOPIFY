8602536e3df396cda8ee1cdf091d6e2c0462a942
gopirajumaruboina <gopirajumaruboina1@gmail.com>
2025-11-12 11:03:50 +0530

    change

--- tests/Blog.spec.ts
+++ tests/Blog.spec.ts
@@ -2,10 +2,10 @@ import { expect, test } from '@playwright/test'
 import { Login } from '../Source/Pages/login'
 import { Blog } from '../Source/Pages/Blog'
 
-test('blog',async({page})=>{
- const login = new Login(page)
- const blog = new Blog(page)
- await login.navigation()
- await blog.BlogNav()
- await expect(await blog.BlogMsg()).toContain('This is your store’s blog. You can use it to talk about new product launches, experiences, tips or other news you want your customers to read about.')
+test('blog', async ({ page }) => {
+    const login = new Login(page)
+    const blog = new Blog(page)
+    await login.navigation()
+    await blog.BlogNav()
+    await expect(await blog.BlogMsg()).toContain('This is your store’s blog. You can use it to talk about new product launches, experiences, tips or other news you want your customers to read about.')
 })
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,10 @@
 import { test } from '@playwright/test'
 import dotenv from 'dotenv'
 import { PerformLogin } from '../Source/utils/CommonFunctions'
+import path from 'path'
 
 dotenv.config()
 test('login', async ({ page }) => {
     await PerformLogin(page, process.env.S_Username!, process.env.S_Password!)
+    await page.context().storageState({path:'auth.json'})
 })
