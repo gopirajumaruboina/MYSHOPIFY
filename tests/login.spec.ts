ae5a251851edd72278b782242a44c0db39e7d80e
gopirajumaruboina <gopirajumaruboina1@gmail.com>
2025-11-12 14:15:09 +0530

    change

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import dotenv from 'dotenv'
 import { PerformLogin } from '../Source/utils/CommonFunctions'
 import path from 'path'
@@ -6,5 +6,6 @@ import path from 'path'
 dotenv.config()
 test('login', async ({ page }) => {
     await PerformLogin(page, process.env.S_Username!, process.env.S_Password!)
-    await page.context().storageState({path:'auth.json'})
+    await expect(page).toHaveScreenshot('Homepage.png',{fullPage:true})
+    //await page.context().storageState({path:'auth.json'})
 })
