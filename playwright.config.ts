2b130195b6d1cb14890a195bce7cc28096d5d73b
gopirajumaruboina <gopirajumaruboina1@gmail.com>
2025-11-12 11:02:51 +0530

    changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
      baseURL: 'https://sauce-demo.myshopify.com/',
-     storageState :'auth.json',
+    //storageState :'auth.json',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
