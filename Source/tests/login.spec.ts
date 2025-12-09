d659cb4de1a57f94309b6b0160a6a0ac87f84d37
gopirajumaruboina <100666844+gopirajumaruboina@users.noreply.github.com>
2025-12-09 13:05:39 +0530

    Update login.spec.ts
    skip the login test

--- Source/tests/login.spec.ts
+++ Source/tests/login.spec.ts
@@ -4,7 +4,7 @@ import { PerformLogin } from '../utils/CommonFunctions'
 import { equal } from 'assert'
 
 dotenv.config()
-test('login', async ({ page }) => {
+test.skip('login', async ({ page }) => {
     await PerformLogin(page, process.env.S_Username!, process.env.S_Password!)
     //await page.pause()
     await expect(page).toHaveScreenshot('Homepage.png',{fullPage:true})
